b713ffcd853093e8d9ba89a0f0a29d21e9afb73a
Adam Grezak <adamgrezak@gmail.com>
2017-01-30 08:57:28 +0100

    table JS behaviour - delete row / cancel deletion

--- src/js/app.js
+++ src/js/app.js
@@ -9,16 +9,36 @@ document.addEventListener("DOMContentLoaded", () => {
       constructor(element) {
         this.element = element;
         this.events(element);
+        this.css = "JS-confirm-action";
       }
 
       events(table) {
         let tableTrigger = qsa("[data-action-trigger]", table);
+        let cancelTrigger = qs("[data-cancel-trigger]");
+
         on(tableTrigger, "click", this.confirmAction.bind(this));
+        on(cancelTrigger, "click", this.cancelDeletion.bind(this));
       }
 
       confirmAction() {
-        let row = event.target.parentNode;
-        this.deleteRow(row);
+        let element = event.target;
+        let row = element.parentNode;
+
+        if(element.classList.contains(this.css)) {
+          this.deleteRow(this.row);
+          return;
+        }
+
+        element.classList.add(this.css);
+
+
+      }
+
+      cancelDeletion() {
+        let rows = qsa(".JS-confirm-action");
+        rows.forEach(i => {
+          i.classList.remove(this.css);
+        });
       }
 
       deleteRow(row) {
